b9913ee93e74c04a9a176653bc795e5ba9026f85
MajedDalain <majed-dalain@hotmail.com>
2019-07-25 22:05:23 +0200

    SPA part_2

--- ReactCourse/confusion/src/components/DishdetailComponent .js
+++ ReactCourse/confusion/src/components/DishdetailComponent .js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Card, CardImg, CardBody, CardText, CardTitle, ListGroup, ListGroupItem } from 'reactstrap';
-
+import { Card, CardImg, CardBody, CardText, CardTitle, ListGroup, ListGroupItem, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 function RenderComments({ comments }) {
     return (
@@ -51,16 +51,26 @@ function RenderDish({ dish }) {
 
 const Dishdetail = (props) => {
     const selectedDish = props.dish;
-    console.log("render is invoked now")
     if (selectedDish != null) {
         return (
             <div className="container">
+                  <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home"> Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem ><Link to="/menu"></Link>Menu</BreadcrumbItem>
+                    <BreadcrumbItem active >{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr/>
+                </div>
+            </div>
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
                         <RenderDish dish={selectedDish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.dish.comments} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
             </div>
